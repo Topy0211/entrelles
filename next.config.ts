e38d8fd3b515a4b2465a1a9621ca0ce0c6f090bf
Topy Rajaonarinosy <topy01rajaonarinosy@gmail.com>
2025-05-31 18:40:04 +0000

    il affiche compiled with warning lorsque j'ai executer npm run build. corrige cet erreur

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     unoptimized: true, // Add this line to disable image optimization
     remotePatterns: [
